6ef2e8b2076525c0ddc96b486c7e5124f8ead885
Sezgin Guel <guel.sezgin@gmail.com>
2019-10-29 22:03:38 +0100

    firststeps

---
+++ script.py
@@ -0,0 +1,16 @@
+import sys
+import requests
+
+
+
+print(sys.version)
+print(sys.executable)
+
+def greet(who):
+  greeting ="Hello, {}".format(who)
+  return greeting
+
+
+
+r = requests.get("https://coreyms.com")
+print(r.status_code)
